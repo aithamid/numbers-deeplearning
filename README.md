2f017f1cbd36ca6e45c40019a99822d5eba63c08
Ait Hamid Adam <aithamidbusiness@gmail.com>
2024-05-16 01:55:52 +0200

    Descente de gradient : 85%

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# numbers-deeplearning
+# Deep Learning numbers recognition
+
+---
+
+Authors : 
+- Adam AIT HAMID
+- Raphaël BOUCHEZ
+
+---
+
+This model is trained on the [digit-recognizer dataset](https://www.kaggle.com/c/digit-recognizer/data?select=sample_submission.csv).
---
+++ train.py
@@ -0,0 +1,120 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# Load the data
+data = pd.read_csv('dataset/train.csv')
+
+# Reorder the data
+data = np.array(data)
+m, n = data.shape
+np.random.shuffle(data)
+
+# Split the data
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+X_dev = X_dev / 255.
+
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n]
+X_train = X_train / 255.
+
+_, m_train = X_train.shape
+
+def relU(Z):
+    return np.maximum(Z, 0)
+
+def relU_prime(Z):
+    return Z > 0
+
+def softMax(Z):
+    A = np.exp(Z) / sum(np.exp(Z))
+    return A
+
+def get_accuracy(predictions, Y):
+    print(predictions, Y)
+    return np.sum(predictions == Y) / Y.size
+
+def get_predictions(A2):
+    return np.argmax(A2, 0)
+
+def make_predictions(X, W1, b1, W2, b2):
+    _, _, _, A2 = forward_propagation(X, W1, b1, W2, b2)
+    predictions = get_predictions(A2)
+    return predictions
+
+def test_prediction(index, W1, b1, W2, b2):
+    current_image = X_train[:, index, None]
+    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+    label = Y_train[index]
+    print("Prediction: ", prediction)
+    print("Label: ", label)
+
+    current_image = current_image.reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
+
+def initialize_parameters():
+    W1 = np.random.rand(10, 784) - 0.5
+    b1 = np.random.rand(10, 1) - 0.5
+    W2 = np.random.rand(10, 10) - 0.5
+    b2 = np.random.rand(10, 1) - 0.5
+    return W1, b1, W2, b2
+
+
+# Forward propagation
+def forward_propagation(X, W1, b1, W2, b2):
+    Z1 = W1.dot(X) + b1
+    A1 = relU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softMax(Z2)
+    return Z1, A1, Z2, A2
+
+# Backward propagation
+def backward_propagation(X, Y, Z1, A1, Z2, A2, W1, W2):
+    dZ2 = A2 - one_hot(Y)
+    dW2 = 1/m * dZ2.dot(A1.T)
+    dB2 = 1/m * np.sum(dZ2)
+    dZ1 = W2.T.dot(dZ2) * relU_prime(Z1)
+    dW1 = 1/m * dZ1.dot(X.T)
+    dB1 = 1/m * np.sum(dZ1)
+    return dW1, dB1, dW2, dB2
+
+# Update the weights
+def update_weights(W1,W2,b1,b2,dW1,dB1,dW2,dB2,alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * dB1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * dB2
+    return W1, W2, b1, b2
+
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+# Gradient descent
+def gradient_descent(X,Y,alpha,iterations):
+    W1, b1, W2, b2 = initialize_parameters()
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forward_propagation(X, W1, b1, W2, b2)
+        dW1, dB1, dW2, dB2 = backward_propagation(X, Y, Z1, A1, Z2, A2, W1, W2)
+        W1, W2, b1, b2 = update_weights(W1, W2, b1, b2, dW1, dB1, dW2, dB2, alpha)
+        if i % 10 == 0:
+            print("Iteration: ", i)
+            predictions = get_predictions(A2)
+            print(get_accuracy(predictions, Y))
+    return W1, W2, b1, b2
+
+# Train the model
+W1, W2, b1, b2 = gradient_descent(X_train, Y_train, 0.10, 500)
+test_prediction(0, W1, b1, W2, b2)
+
+
+
+
+
